f5fb4c528195b954e7eee8b79b88d0f1abe8a3cd
nikolai <cnlopez1@hotmail.com>
2024-09-27 18:51:27 -0500

    crud projects

---
+++ TaskManagementUI/TaskManagementUI/Models/TaskTable.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagementUI.Models
+{
+    public class TaskTable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public int Priority { get; set; }
+        public DateTime DueDate { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
