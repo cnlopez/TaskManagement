d8f465d57cee17d3c46a60f6e6f5331305be7dc3
nikolai <cnlopez1@hotmail.com>
2024-09-29 16:20:49 -0500

    More in IU

--- TaskManagementUI/TaskManagementUI/Models/TaskTable.cs
+++ TaskManagementUI/TaskManagementUI/Models/TaskTable.cs
@@ -1,13 +1,20 @@
-﻿namespace TaskManagementUI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementUI.Models
 {
     public class TaskTable
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The task title is mandatory.")]
         public string Title { get; set; }
+
         public string Description { get; set; }
         public string Status { get; set; }
         public int Priority { get; set; }
         public DateTime DueDate { get; set; }
+
+        [Required(ErrorMessage = "The project is mandatory.")]
         public int ProjectId { get; set; }
     }
 }
