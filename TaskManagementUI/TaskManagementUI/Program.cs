4dc19c9e5e1d9efb9ea668eef48b121dba59cc32
nikolai <cnlopez1@hotmail.com>
2025-03-13 06:38:15 -0500

    Fix environment

--- TaskManagementUI/TaskManagementUI/Program.cs
+++ TaskManagementUI/TaskManagementUI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http.Json;
 using TaskManagementUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,7 +9,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"];
+Console.WriteLine($"API Base URL: {apiBaseUrl}");  // Debug
+
+//var settings = await httpClient.GetFromJsonAsync<Dictionary<string, string>>("appsettings.json");
+//var apiBaseUrl = settings?["ApiBaseUrl"] ?? "https://localhost:7072/";
+
 
 // Set HttpClient to comunication with backend
 builder.Services.AddScoped(sp =>
